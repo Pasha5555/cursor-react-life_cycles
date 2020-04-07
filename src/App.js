daf4220fa61b131b73c5ca63baa7b13c036c986e
Pavlo <pavloterleckij2@gmail.com>
2020-04-07 18:17:19 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -7,19 +7,19 @@ import Timer from './Component'
 class App extends Component{
   
   render(){
-  return (
-    <div className="App">
-      <Timer 
-      time={30} 
-      step={1} 
-      autostart={true}
-      onTick={(time) => console.log("Осталось времени: " + time)} 
-      onTimeEnd={() => console.log("Время вышло!")}
-      onTimeStart={(timeLeft) => console.log("Таймер запущен!")}
-      onTimePause={(timeLeft) => console.log("Таймер на паузе!")}
-      />
+    return (
+      <div className="App">
+        <Timer 
+        time={30} 
+        step={1} 
+        autostart={true}
+        onTick={(time) => console.log("Осталось времени: " + time)} 
+        onTimeEnd={() => console.log("Время вышло!")}
+        onTimeStart={(timeLeft) => console.log("Таймер запущен!")}
+        onTimePause={(timeLeft) => console.log("Таймер на паузе!")}
+        />
     </div>
-  );
+    );
   }
 }
 
